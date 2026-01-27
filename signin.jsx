7b0b4955843efd1e937013280cc68f7d815582e3
Esteban Dufau <esteban.dufau@ynov.com>
2026-01-27 16:51:13 +0100

    Update signin.jsx with new content

---
+++ signin.jsx
@@ -0,0 +1,83 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+
+import "../App.css";
+import styles from "./signin.module.css";
+
+export default function Login() {
+    const nav = useNavigate();
+    const [msg, setMsg] = useState("");
+
+    async function sendCampagin(event) {
+        event.preventDefault();
+        const form = new FormData(event.target);
+        const email = form.get("email");
+        const password = form.get("password");
+
+        if (email == "" || password == "") {
+            alert("Veuillez remplir le/les champ(s) !");
+        } else {
+            if (email.search("@") == -1) {
+                alert("Veuillez entrer une adresse email valide !");
+            } else {
+                if (
+                    email.includes("=") ||
+                    email.includes(";") ||
+                    email.includes("(") ||
+                    email.includes(")") ||
+                    email.includes(\"\")
+                ) {
+                    alert("Veuillez ne pas utiliser de caractère spécial !");
+                } else {
+                    setMsg(
+                        await invoke("sendLoginForm", {
+                            email: email,
+                            password: password,
+                        })
+                    );
+                }
+            }
+        }
+    }
+
+    async function login(event) {
+        event.preventDefault();
+        const tokens = await invoke("login");
+        console.log(tokens);
+        const parsed =
+            typeof tokens === "string" ? JSON.parse(tokens) : tokens;
+        localStorage.setItem("Phishlab_Token", JSON.stringify(parsed));
+        nav("/CampaignBuilder");
+    }
+
+    return (
+        <main className="container">
+            <h1>Page Connexion de Compte</h1>
+            <form className={styles.form} onSubmit={login}>
+                <p className={styles.emailP}>
+                    Email : <input className={styles.email} name="email" />
+                </p>
+                <p className={styles.passwordP}>
+                    Mot de passe :
+                    <input
+                        className={styles.password}
+                        type="password"
+                        name="password"
+                    />
+                </p>
+                <button className={styles.submit} type="submit">
+                    Envoyer
+                </button>
+            </form>
+            <br />
+            <p>{msg}</p>
+            <p>
+                Vous n'avez pas de compte ?
+                <button type="button" onClick={() => nav("/register")}>
+                    Inscrivez vous
+                </button>
+            </p>
+        </main>
+    );
+}
